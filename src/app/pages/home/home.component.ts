6fbe89a476abe304e1de8303d125320795e041ce
ShoxruxTeacher <yarashovshokhrukh@gmail.com>
2025-01-18 01:17:16 +0500

    sign in and sign up add with profile page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,7 +5,7 @@ import {HeaderComponent} from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent,FooterComponent,NavbarComponent],
+  imports: [HeaderComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
