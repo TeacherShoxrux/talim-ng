c813cc212381656a17ec5c504b1a150a52da3181
ShoxruxTeacher <yarashovshokhrukh@gmail.com>
2025-01-17 22:22:32 +0500

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
 
 export const routes: Routes = [];
+const routesConfig: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home Page'
+  },
+
+]
+export default routesConfig;
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {FooterComponent} from '../../components/footer/footer.component';
+import {NavbarComponent} from '../../components/navbar/navbar.component';
+import {HeaderComponent} from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderComponent,FooterComponent,NavbarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
