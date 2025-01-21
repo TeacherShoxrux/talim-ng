0603ef0cc4ccb7943f656b573124603501b933b4
ShoxruxTeacher <yarashovshokhrukh@gmail.com>
2025-01-21 20:01:30 +0500

    theme-button component add

---
+++ src/app/components/theme-button/theme-button.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-theme-button',
+  imports: [],
+  templateUrl: './theme-button.component.html',
+  styleUrl: './theme-button.component.css'
+})
+export class ThemeButtonComponent {
+  @Input()
+  buttonName: string | undefined;
+}
--- src/app/pages/subject-themes/subject-themes.component.ts
+++ src/app/pages/subject-themes/subject-themes.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from '../../components/header/header.component';
 import {FooterComponent} from '../../components/footer/footer.component';
 import {RouterLink} from '@angular/router';
+import {ThemeButtonComponent} from '../../components/theme-button/theme-button.component';
 
 @Component({
   selector: 'app-subject-themes',
   imports: [
     HeaderComponent,
     FooterComponent,
-    RouterLink
+    ThemeButtonComponent
   ],
   templateUrl: './subject-themes.component.html',
   styleUrl: './subject-themes.component.css'
